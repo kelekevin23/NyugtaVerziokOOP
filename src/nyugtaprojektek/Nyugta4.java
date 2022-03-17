74835212069a60d3ae37a08249ee63525d4c6e2f
csoltip <csolti@szamalk-szalezi.hu>
2022-03-17 12:30:11 +0100

    kinézet jó: vonal(char, int, boolean sortores)

--- src/nyugtaprojektek/Nyugta4.java
+++ src/nyugtaprojektek/Nyugta4.java
@@ -2,9 +2,9 @@ package nyugtaprojektek;
 public class Nyugta4 {
 
     public static void main(String[] args) {
-        vonal('*', 20);
+        vonal('*', 20, true);
         System.out.printf("%14s\n", "Nyugta 4");
-        vonal('*', 20);
+        vonal('*', 20, true);
         
         final String HUF = "Ft";
         int tetelek[] = {350, 90, 1320};
@@ -13,7 +13,7 @@ public class Nyugta4 {
             System.out.printf("%10s: %5d %s\n", "Tétel 1", tetelek[i], HUF);
         }
 
-        vonal('=', 20);
+        vonal('=', 20, true);
         
         //összegzés tétele:
         int osszesen = 0;
@@ -21,17 +21,15 @@ public class Nyugta4 {
             osszesen += tetelek[i];
         }
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
-        
-        //String szaggatottVonal = "--------------------";
-        //System.out.println(szaggatottVonal);
-        vonal('-', 20);
+       
+        vonal('-', 20, true);
         
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
         System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
         
-        vonal('=', 20);
+        vonal('=', 20, true);
 
         int fizetendo = osszesen + szervizDij;
         System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
@@ -40,26 +38,25 @@ public class Nyugta4 {
         
         System.out.printf("%10s%7.2f %s\n","", euro, eur);//
         
-        vonal('-', 20);
+        vonal('-', 20, true);
         
         System.out.println("");
-        //rovidVonal();
-        vonal('_', 7);
+
+        vonal('_', 7, false);
         String rovidVonalValaszto = "      ";
         System.out.print(rovidVonalValaszto);
-        //rovidVonal();
-        vonal('_',7);
-        System.out.println("");
+        vonal('_',7, true);
+        //System.out.println("");
         System.out.print(" Dátum");
         System.out.print(rovidVonalValaszto);
         System.out.println("   Név");
   
-        vonal('*', 20);
+        vonal('*', 20, true);
         System.out.println("        CÉG");
-        vonal('*', 20);
+        vonal('*', 20, true);
     }
 
-    private static void vonal(char jel, int hossz) {
+    private static void vonal(char jel, int hossz, boolean sortores) {
         String vonal = "";
         
         //összegzés tétele:
@@ -68,18 +65,11 @@ public class Nyugta4 {
             vonal += jel;
         }
         
-        System.out.println(vonal);
+        //System.out.println(vonal);
+        System.out.print(vonal);
+        
+        if(sortores){
+            System.out.println("");
+        }
     }
-
-//    private static void rovidVonal() {
-//        String vonal = "";
-//        
-//        //összegzés tétele:
-//        for (int i = 0; i < 7; i++) {
-//            //vonal += "*";
-//            vonal += '_';
-//        }
-//        
-//        System.out.print(vonal);
-//    }
 }
