f181d449aa4b3cc4b05d66254ce8d843e610c290
csoltip <csolti@szamalk-szalezi.hu>
2022-03-17 12:14:14 +0100

    csillagok()

--- src/nyugtaprojektek/Nyugta4.java
+++ src/nyugtaprojektek/Nyugta4.java
@@ -2,16 +2,14 @@ package nyugtaprojektek;
 public class Nyugta4 {
 
     public static void main(String[] args) {
-        String csillagok = "********************";
-        System.out.println(csillagok);
+        //String csillagok = "********************";
+        //System.out.println(csillagok);
+        csillagok();
         System.out.printf("%14s\n", "Nyugta 4");
-        System.out.println(csillagok);
+        //System.out.println(csillagok);
+        csillagok();
         
         final String HUF = "Ft";
-//        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
-//        System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
-//        System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
-//        System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
         int tetelek[] = {350, 90, 1320};
         int tetelDb = tetelek.length;
         for (int i = 0; i < tetelDb; i++) {
@@ -22,7 +20,7 @@ public class Nyugta4 {
         System.out.println(duplaVonal);
         
         //összegzés tétele:
-        int osszesen = 0; //tetel1 + tetel2 + tetel3;
+        int osszesen = 0;
         for (int i = 0; i < tetelDb; i++) {
             osszesen += tetelek[i];
         }
@@ -57,8 +55,21 @@ public class Nyugta4 {
         System.out.print(rovidVonalValaszto);
         System.out.println("   Név");
         
-        System.out.println(csillagok);    
+        //System.out.println(csillagok);    
+        csillagok();
         System.out.println("        CÉG");
+        //System.out.println(csillagok);
+        csillagok();
+    }
+
+    private static void csillagok() {
+        String csillagok = "";
+        
+        //összegzés tétele:
+        for (int i = 0; i < 20; i++) {
+            csillagok += "*";
+        }
+        
         System.out.println(csillagok);
     }
 
