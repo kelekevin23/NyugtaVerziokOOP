1ec84545475def193c5178dddde035527662b5d9
kelekevin23 <kelekevin11@gmail.com>
2022-03-23 11:43:14 +0100

    nyugta3 szerkesztése

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -1,9 +1,10 @@
 package nyugtaprojektek;
+
 /* Probléma:
 tüntessük el a kézzel beírt szóközöket,
 a huf ne tartalmazzon eurót
 a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzeghez
-*/
+ */
 public class Nyugta3 {
 
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Nyugta3 {
         //System.out.println("     Nyugta 3");
         System.out.printf("%14s\n", "Nyugta 3");
         System.out.println(csillagok);
-        
+
         final String HUF = "Ft";
         int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
 //        System.out.printf("Tétel 1:     %d %s\n", tetel1, huf);
@@ -24,22 +25,22 @@ public class Nyugta3 {
 
         String duplaVonal = "====================";
         System.out.println(duplaVonal);
-        
+
         int osszesen = tetel1 + tetel2 + tetel3;
         //System.out.printf("Összesen:   %d %s\n", osszesen, huf);
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
-        
+
         String szaggatottVonal = "--------------------";
         System.out.println(szaggatottVonal);
-        
+
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
         //System.out.printf("Kedvezmény:  %d %s\n", kedvezmeny, huf);
         System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
-        
+
         System.out.println(duplaVonal);
-        
+
         //int fizetendo = osszesen - szervizDij;
         int fizetendo = osszesen + szervizDij;
         //System.out.printf("Fizetendő:  %d %s\n", fizetendo, huf);
@@ -48,26 +49,23 @@ public class Nyugta3 {
         //HUF = "\u20ac";//EZ NEM JÓ!!!!
         final String eur = "\u20ac";
         //System.out.printf("            %f %s\n", euro, eur);
-        
+
         /* 7.2 7 szélesen 2 tizedessel,a max: 1234.99
         *  a %10s  egy "" -t ír ki, ezzel tolom beljebb
-        */
-        System.out.printf("%10s%7.2f %s\n","", euro, eur);//
-        
-        System.out.println(szaggatottVonal);
-        
-        System.out.println("");
+         */
+        System.out.printf("%10s%7.2f %s\n", "", euro, eur);//
+
         String rovidVonal = "_______";
+        System.out.println(szaggatottVonal + "\n");
+
         System.out.print(rovidVonal);
-        String rovidVonalValaszto = "      ";
-        System.out.print(rovidVonalValaszto);
-        System.out.println(rovidVonal);
-        System.out.print(" Dátum");
-        System.out.print(rovidVonalValaszto);
-        System.out.println("   Név");
-        
-        System.out.println(csillagok);    
-        System.out.println("        CÉG");
+        System.out.printf("%13s\n", rovidVonal);
+
+        System.out.printf("%6s", "Dátum");
+        System.out.printf("%12s\n", "Név");
+
+        System.out.println(csillagok);
+        System.out.printf("%11s\n", "CÉG");
         System.out.println(csillagok);
     }
 
