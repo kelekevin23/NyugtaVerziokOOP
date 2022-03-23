15d7baed9e8199e2ecb9685f1e05b678f990a509
kelekevin23 <kelekevin11@gmail.com>
2022-03-23 11:44:01 +0100

    nyugta3 kód rendszerezése

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -8,54 +8,40 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 public class Nyugta3 {
 
     public static void main(String[] args) {
+        
+        final String HUF = "Ft";
+        final String eur = "\u20ac";
+        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
+        int osszesen = tetel1 + tetel2 + tetel3;
+        int szervizDijMertek = 10;
+        
         String csillagok = "********************";
+        String duplaVonal = "====================";
+        String szaggatottVonal = "--------------------";
+        String rovidVonal = "_______";
+        
         System.out.println(csillagok);
-        //System.out.println("     Nyugta 3");
         System.out.printf("%14s\n", "Nyugta 3");
         System.out.println(csillagok);
 
-        final String HUF = "Ft";
-        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
-//        System.out.printf("Tétel 1:     %d %s\n", tetel1, huf);
-//        System.out.printf("Tétel 1:      %d %s\n", tetel2, huf);
-//        System.out.printf("Tétel 3:    %d %s\n", tetel3, huf);
-        System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
+        System.out.printf("%10s:%6d%3s\n", "Tétel 1", tetel1, HUF);
+        System.out.printf("%10s:%6d%3s\n", "Tétel 2", tetel2, HUF);
+        System.out.printf("%10s:%6d%3s\n", "Tétel 3", tetel3, HUF);
 
-        String duplaVonal = "====================";
         System.out.println(duplaVonal);
-
-        int osszesen = tetel1 + tetel2 + tetel3;
-        //System.out.printf("Összesen:   %d %s\n", osszesen, huf);
-        System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
-
-        String szaggatottVonal = "--------------------";
+        System.out.printf("%10s:%6d%3s\n", "Összesen", osszesen, HUF);
         System.out.println(szaggatottVonal);
 
-        int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
-        //System.out.printf("Kedvezmény:  %d %s\n", kedvezmeny, huf);
-        System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
+        System.out.printf("%10s:%6d%3s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
-
         System.out.println(duplaVonal);
 
-        //int fizetendo = osszesen - szervizDij;
         int fizetendo = osszesen + szervizDij;
-        //System.out.printf("Fizetendő:  %d %s\n", fizetendo, huf);
-        System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
+        System.out.printf("%10s:%6d%3s\n", "Fizetendő", fizetendo, HUF);
+        
         double euro = fizetendo / 350.0;
-        //HUF = "\u20ac";//EZ NEM JÓ!!!!
-        final String eur = "\u20ac";
-        //System.out.printf("            %f %s\n", euro, eur);
-
-        /* 7.2 7 szélesen 2 tizedessel,a max: 1234.99
-        *  a %10s  egy "" -t ír ki, ezzel tolom beljebb
-         */
-        System.out.printf("%10s%7.2f %s\n", "", euro, eur);//
-
-        String rovidVonal = "_______";
+        System.out.printf("%10s%7.2f%2s\n", "", euro, eur);//
         System.out.println(szaggatottVonal + "\n");
 
         System.out.print(rovidVonal);
